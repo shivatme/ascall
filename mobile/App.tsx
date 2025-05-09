23665d4dc99d4e797a75c496779d4ae1f36a2c91
Shivam5278 <st527874@gmail.com>
2025-05-09 10:42:06 +0530

    Downgraded to expo sdk 52

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,27 +1,20 @@
-import React, { useEffect } from "react";
-import { Text, View } from "react-native";
-import { socket } from "./src/socket";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  useEffect(() => {
-    console.log("Connecting to server...");
-    socket.on("connect", () => {
-      console.log("Connected to server:", socket.id);
-      socket.emit("join", "room-123");
-    });
-
-    socket.on("user-joined", (userId) => {
-      console.log("Another user joined:", userId);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Video Call App</Text>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- mobile/src/socket.ts
+++
@@ -1,3 +0,0 @@
-import { io } from "socket.io-client";
-
-export const socket = io("http:192.168.29.162:3010"); // Replace with LAN IP of your backend
